d7fbec4ca88d042fa2b710a8090d060a5dd6dad3
b4dr47 <me@b4dr47.dev>
2026-04-29 22:04:32 +0800

    add more hints

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,23 @@
 
   const stage = $derived(form?.stage ?? data.stage);
 
+  let showHint = $state(0);
+  const hints: Record<number, string> = {
+    1: 'b4ss_8^2',
+    2: 'https://slides.com/ying_ouob/code#/2/7',
+    3: 'Google what is RSA encryption',
+  };
 </script>
 
 <div class="min-h-screen flex items-center justify-center bg-gray-900">
   <div class="w-full max-w-md text-gray-300 bg-gray-800 p-10 rounded-2xl space-y-8">
   <h1 class="text-5xl font-bold">Stage {stage}</h1>
-
+  <button class="hint-btn font-bold" onclick={() => showHint = !showHint}>
+    {showHint ? 'Hide Hint' : 'Show Hint'}
+  </button>
+  {#if showHint}
+    <p class="hint font-bold">{hints[stage]}</p>
+  {/if}
   <form method="POST" action="?stage={stage}">
     <input
       type="flag"
